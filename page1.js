13757b64a6a8be7906d02b1bfc132a10bfa3e067
Artex6666 <69062885+Artex6666@users.noreply.github.com>
2021-02-10 07:53:39 +0100

    Add files via upload

--- page1.js
+++ page1.js
@@ -1,10 +1,13 @@
-const oracle = document.querySelector("header h1")
-const titre1 = document.querySelector(".wrapper")
-window.addEventListener("load", () => {
-    const TL = gsap.timeline({paused:true});
-    
-    TL.staggerFrom(oracle, 1, {opacity: 0, ease: "power2.out"},0.3)
-    TL.staggerFrom(titre1, 1, {top:-50, opacity: 0, ease: "power2.out"},0.3)
-    
+const l1 = document.querySelector('.l1');
+const titreSpans = document.querySelectorAll('main span');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .staggerFrom(titreSpans, 1.5, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
+
     TL.play();
 })
